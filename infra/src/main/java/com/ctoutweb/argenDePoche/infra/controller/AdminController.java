8e8c3370c734820cd4a464793db4dcc433b66cdf
cyrille <aviateur22@hotmail.fr>
2026-01-26 21:21:08 +0100

    commit du 26-01-2026: Modification du VERBE GET en POST pour l'appelle au service de l'initialisation des périodes

--- infra/src/main/java/com/ctoutweb/argenDePoche/infra/controller/AdminController.java
+++ infra/src/main/java/com/ctoutweb/argenDePoche/infra/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -22,7 +23,7 @@ public class AdminController {
     this.adminService = adminService;
   }
 
-  @GetMapping("/initialize-next-period")
+  @PostMapping("/initialize-next-period")
   public Mono<ResponseEntity<String>> initializeNextPeriod() {
     LOGGER.info(() -> "Initialisation de la période suivante");
     return adminService.initializeNextPeriod()
