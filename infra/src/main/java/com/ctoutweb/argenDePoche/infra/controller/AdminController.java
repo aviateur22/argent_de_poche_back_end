b1b7b307a13a4aab324913572568eb409de622b3
cyrille <aviateur22@hotmail.fr>
2026-01-26 21:21:51 +0100

    commit du 26-01-2026: Modification du VERBE GET en POST pour l'appelle au service de l'initialisation des périodes

--- infra/src/main/java/com/ctoutweb/argenDePoche/infra/controller/AdminController.java
+++ infra/src/main/java/com/ctoutweb/argenDePoche/infra/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.ctoutweb.argenDePoche.infra.service.AdminService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
